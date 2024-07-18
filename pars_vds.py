68449a3a3b42b05ecb4a9cccf338f7af35bf9758
ebikdeli <ebikdeli@gmail.com>
2024-07-18 17:32:06 +0330

    pars vds updated

--- pars_vds.py
+++ pars_vds.py
@@ -1,12 +1,20 @@
 from drivers.chrome import start_driver
+from selenium.webdriver.common.by import By
 import time
 
 
 
 def start(wait:int=0) -> None:
     """Start scraping ParsVDS"""
-    driver = start_driver()
+    driver = start_driver(False)
     if driver is not None:
-        driver.get('https://www.zoomg.ir/cinema-articles/369521-presumed-innocent-tv-show-introduction/')
-        time.sleep(wait)
+        driver.get('https://parsvds.com/virtual-server/iran-ssd/')
+        # time.sleep(wait)
+        prices = []
+        prices_elem = driver.find_elements(By.CSS_SELECTOR, '.server-order-button')
+        for pe in prices_elem:
+            price = pe.text.replace(',', '')
+            for p in price.split(' '):
+                if p.isdigit():
+                    print(int(p))
         driver.close()
